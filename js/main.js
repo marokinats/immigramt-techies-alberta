863f563b34c1c43ae411360c71aedd731778235a
Tania Marokina <tania.marokina@gmail.com>
2023-10-30 07:01:17 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
 (function ($) {
   'use strict';
 
-  /*PRELOADER JS
-	$(window).on('load', function() { 
-		$('.loader').fadeOut();
-		$('.preloader').delay(350).fadeOut('slow'); 
-	}); 
-END PRELOADER JS*/
-
-  $(window).on('scroll', function () {
+  function menuShrink() {
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('menu-shrink');
     } else {
       $('#header').removeClass('menu-shrink');
     }
-  });
+  }
+
+  $(window).on('load', menuShrink);
+  $(window).on('scroll', menuShrink);
 
   const links = document.querySelectorAll('.header__link');
   for (const link of links) {
     link.addEventListener('click', clickHandler);
   }
+
   function clickHandler(e) {
     e.preventDefault();
     const href = this.getAttribute('href');
-    const offsetTop = document.querySelector(href).offsetTop - 100;
+    const offsetTop = document.querySelector(href).offsetTop - 85;
     scroll({
       top: offsetTop,
       behavior: 'smooth'
     });
   }
 
-  $(document).on('click', '.navbar-collapse.in', function (e) {
-    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+  $(document).on('click', '.navbar-collapse.show', function (e) {
+    console.log(e.target);
+    if ($(e.target).is('a')) {
       $(this).collapse('hide');
     }
   });
